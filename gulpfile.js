b4eecb3f2198a9f5883823acb1b412e3c784da21
Hong Nguyen <nghongbk@gmail.com>
2017-05-13 09:43:16 +0700

    Updated archive link to homepage

--- gulpfile.js
+++ gulpfile.js
@@ -100,6 +100,6 @@ gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() 
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('./**/*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
 });
